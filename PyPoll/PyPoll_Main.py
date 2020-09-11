4591613da097acb3241a37bcb2df5cc4fd15bfd3
Louis Razuki <louisrazuki@louis.localdomain>
2020-09-11 21:27:10 +1000

    Finishing touches

--- PyPoll/PyPoll_Main.py
+++ PyPoll/PyPoll_Main.py
@@ -4,31 +4,59 @@ import csv
 
 # Open data file
 csvpath = os.path.join("..","PyPoll","Resources","election_data.csv")
-
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile,delimiter=",")
     
+    # skip the header
     csv_header = next(csvreader)
 
+    #establish an empty list
     candidates = {}
 
-    for row in csvreader:
+    for row in csvreader: 
+        
+        # setting the name as variable
         name = row[2]
 
-        # opening the counter to define the value pair
+        # defining the {candidate: vote count} value pair
         if name not in candidates:
             candidates[name] =  1
         else:
             candidates[name] += 1
-
-    print(candidates)
     
-    # for key, value in candidates.items()
+    # counting the total votes
+    total_votes = sum(candidates.values())
 
-    # take the key and value of the dictionary and turn in to tuple
+    # sorting the list based on vote count
+    sorted_values = sorted(candidates.values())
+    sorted_keys = (candidates.keys())
+    sorted_values.reverse()
+    
+    # finding the winning candidate name
+    winner = max(candidates, key=candidates.get)
 
-    # find the max value from this and return the key
+
+# preparing to write the csv
+output_path = os.path.join("..","PyPoll","Output","output.txt")
+
+results = open(output_path,"w+")
+
+# writing the winner
+results.write("The winner is " + winner + "\n")
+
+results.write("=========================\n")
+
+# using a for loop to cycle through the candidates and print their values & percentage
+for key, value in zip(sorted_keys, sorted_values):
+        results.write(f"{key}: {value}" + " (%.2f%%)" % (100*float(value)/total_votes)+ "\n")
+    
+results.write("=========================\n")
+
+# tallying the final votes and writing
+results.write("There were " + str(total_votes) + " votes cast\n")
+
+
 
 
 
